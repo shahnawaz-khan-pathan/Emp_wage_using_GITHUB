ae58bf4ed20bbcf7337c9d43e288bffb9bfa2b64
shahnawaz98 <ksahib1735@gmail.com>
2021-03-19 19:23:41 +0530

    calculate_emp_wage

--- welcome.sh
+++ welcome.sh
@@ -29,4 +29,20 @@ fi
 totalsalary=$(($totalsalary*$salary))
 done
 echo $totalsalary
-
+isparttime=1
+isfulltime=2
+empperhr=20
+empchk=$((RANDOM%3))
+case $empchk in
+$isfulltime)
+emphrs=8
+;;
+$isparttime)
+emphrs=4
+;;
+*)
+emphrs=0
+;;
+esac
+salary=$((emphrs*empperhr))
+echo $salry
